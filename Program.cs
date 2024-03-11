f09ee5a00e7470127a69f3093f42afebc5da7e27
Eleaz <Eleaz@THEGOATPC>
2024-03-11 18:27:58 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace GenericAnimalClass
+{
+    public class Animal < T>
+    {
+        public T data;
+
+        public Animal(T data)
+        {
+            this.data = data;
+        }
+        public T getAnimal()
+        {
+            Console.WriteLine("Data Passed: " + data);
+            return data;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal<string> animalName = new Animal<string>("Cow");
+            Animal<string> animalHabitat = new Animal<string>("Farm");
+            Animal<bool> endangered = new Animal<bool>(false);
+            Animal<bool> extinct = new Animal<bool>(false);
+            Animal<int> averageLifespan = new Animal<int>(20);
+
+            animalName.getAnimal();
+            animalHabitat.getAnimal();
+            endangered.getAnimal();
+            extinct.getAnimal();
+            averageLifespan.getAnimal();
+        }
+    }
+}
